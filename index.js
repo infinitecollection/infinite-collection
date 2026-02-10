79d7f45c044b3b9b07be83bf1327f751dc30fd59
infinitecollection <infinite151709@gmail.com>
2026-02-10 07:38:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,4 +82,8 @@ app.post("/verify-payment", (req, res) => {
 
 /* ================= START ================= */
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running"));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
+});
+
